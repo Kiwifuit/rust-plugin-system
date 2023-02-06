f43684d830b04265f479dc6c06eea0ce0c924ef6
Kiwifuit <mahkiwi123@gmail.com>
2023-02-06 11:24:21 +0800

    Made the plugin compile to a `cdylib` and used `Plugin`'s functions

--- plugin-runner/src/main.rs
+++ plugin-runner/src/main.rs
@@ -2,4 +2,12 @@ mod loader;
 
 fn main() {
     let plugin = loader::load_plugin("plugin").unwrap();
+
+    println!(
+        "Plugin {} v{} initialized",
+        plugin.get_name(),
+        plugin.get_version()
+    );
+    plugin.start();
+    plugin.stop();
 }
